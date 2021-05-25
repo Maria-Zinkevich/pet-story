47f9d52d66a53db0bad00f1005551eee715b818b
Maria Zinkevich <zinkevichmaryia@gmail.com>
2021-05-25 23:46:35 +0300

    feat: add slider in pets block

--- source/js/landing.js
+++ source/js/landing.js
@@ -14,3 +14,19 @@ document.addEventListener('click', (event) => {
         feedbackPopup.classList.add('hidden');
     }
 });
+
+// Pets slider
+const buttonLeft = document.querySelector('.pets__button--left');
+const buttonRight = document.querySelector('.pets__button--right');
+const petsCards= document.querySelector('.pets__cards');
+let petsCard = document.querySelectorAll('.pets__card');
+
+buttonLeft.addEventListener('click', () => {
+    petsCards.append(petsCard[0]);
+    petsCard = document.querySelectorAll('.pets__card');
+})
+
+buttonRight.addEventListener('click', () => {
+    petsCards.prepend(petsCard[petsCard.length - 1]);
+    petsCard = document.querySelectorAll('.pets__card');
+})
