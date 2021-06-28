673dd9e0f1909935ffb38ee4e5343ae4ffaf6f05
Maria Zinkevich <zinkevichmaryia@gmail.com>
2021-06-28 12:30:02 +0300

    feat: add the functionality of the appearance of a donate-popup when the button is clicked

--- source/js/donate.js
+++ source/js/donate.js
@@ -1,3 +1,20 @@
+// Donate-popup open
+
+const donateButton = document.querySelector('.donate-section__button');
+const donatePopup = document.querySelector('.donate-modal__wrap');
+
+document.addEventListener('click', (event) => {
+    if (event.target.classList.contains('donate-section__button')) {
+        donatePopup.classList.remove('hidden');
+    } 
+    
+    if (event.target.classList.contains('donate-modal__wrap')) {
+        donatePopup.classList.add('hidden');
+    }
+});
+
+// Displaying value of radio buttons in the input field
+
 const donateForm = document.querySelector('.donate-section__form');
 const targetAmount = document.querySelectorAll('.donate-section__radio-button .donate-section__label');
 const amountInput = document.querySelector('.donate-section__input');
