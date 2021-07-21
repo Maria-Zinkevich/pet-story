62b8b03cbe0ca834ea9dd894cce798bf255b0ba6
Maria Zinkevich <zinkevichmaryia@gmail.com>
2021-07-21 13:14:29 +0300

    feat: add implementation of opening a menu on click

--- source/js/map.js
+++ source/js/map.js
@@ -1,3 +1,45 @@
+// Burger menu
+
+const openMenuButton = document.querySelector('.header__burger-menu');
+const closeMenuButton = document.querySelector('.header__nav > div > span');
+const header = document.querySelector('.header');
+const headerNavigation = document.querySelector('.header__nav');
+const headerNavWrap = document.querySelector('.header__nav > div');
+const headerNavLogo = document.querySelector('.header__nav > div > a');
+const headerList = document.querySelector('.header__list');
+const headerItems = document.querySelectorAll('.header__item');
+const headerLinks = document.querySelectorAll('.header__link');
+
+openMenuButton.addEventListener('click', (event) => {
+    header.classList.add('header--open');
+    headerNavigation.classList.add('header__nav--open');
+    headerList.classList.add('header__list--open');
+    headerNavWrap.style.display = "block";
+
+    for (let item of headerItems) {
+        item.classList.add('header__item--open');
+    }
+
+    for (let link of headerLinks) {
+        link.classList.add('header__link--open');
+    }
+});
+
+closeMenuButton.addEventListener('click', (event) => {
+    header.classList.remove('header--open');
+    headerNavigation.classList.remove('header__nav--open');
+    headerList.classList.remove('header__list--open');
+    headerNavWrap.style.display = "none";
+
+    for (let item of headerItems) {
+        item.classList.remove('header__item--open');
+    }
+
+    for (let link of headerLinks) {
+        link.classList.remove('header__link--open');
+    }
+});
+
 // Map zooming
 
 const ZOOM_IN_BUTTON = document.getElementById('zoom-in');
